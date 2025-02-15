efc8303a5086d1d3203e9f3e4d1a1e1946ad42e8
umm e habiba <161445850+UmmeHabiba1312@users.noreply.github.com>
2025-02-16 00:38:51 +0500

    Update ai-technology.tsx

--- src/components/ai-technology.tsx
+++ src/components/ai-technology.tsx
@@ -40,7 +40,7 @@ export default function AITechnologySection() {
         initial={{ opacity: 0, y: -30 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.8 }}
-        className='text-5xl font-extrabold tracking-wide mb-12 relative z-10 text-cyan-400'
+        className='text-3xl sm:text-5xl font-extrabold tracking-wide mb-12 relative z-10 text-cyan-400'
       >
         AI Technology Revolution
       </motion.h2>
